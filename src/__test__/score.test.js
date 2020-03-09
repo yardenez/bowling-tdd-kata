8e1c42ee05f1c0154278547e7742ce977b957b30
Yarden Ezra <yardene@wix.com>
2020-03-09 11:32:17 +0200

    test single strike

--- src/__test__/score.test.js
+++ src/__test__/score.test.js
@@ -13,9 +13,13 @@ describe('Bowling', () => {
     expect(score(rolls)).toBe(8);
   });
 
-  it('should score a game with spare only',()=>{
+  it('should score a game with a single spare',()=>{
     const rolls = [ 5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     expect(score(rolls)).toBe(16);
   });
 
+  it('should score a game with a single strike',()=>{
+    const rolls = [ 10, 0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(score(rolls)).toBe(24);
+  });
 });
--- src/score.js
+++ src/score.js
@@ -3,13 +3,17 @@ export default function score(rolls) {
   let score = 0;
   let frameIndex = 0;
 
-  const isStrike = (frameIndex) => rolls[frameIndex] + rolls[frameIndex+1] === 10;
+  const isStrike = (frameIndex) => rolls[frameIndex] === 10;
+  const isSpare = (frameIndex) => rolls[frameIndex] + rolls[frameIndex+1] === 10;
 
   for(let frame=0; frame < 10; frame++){
     if(isStrike(frameIndex)){
+      score += 10 + rolls[frameIndex+2] + rolls[frameIndex+3];
+    }
+    else if (isSpare(frameIndex)){
       score += 10 + rolls[frameIndex+2];
     }
-    else{
+    else {
       score += rolls[frameIndex]+rolls[frameIndex+1];
     }    
     frameIndex+=2;
